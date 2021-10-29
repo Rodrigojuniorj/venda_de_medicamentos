4f13b8eadffeea3527687d8e22a8510f638e43d3
rodrigo_juniorj <71649665+Rodrigojuniorj@users.noreply.github.com>
2021-10-29 20:18:03 -0300

    primeiro commit

---
+++ src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/VendaDeMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.nosso.grupo.venda_de_medicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaDeMedicamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaDeMedicamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/VendaDeMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.nosso.grupo.venda_de_medicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaDeMedicamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
