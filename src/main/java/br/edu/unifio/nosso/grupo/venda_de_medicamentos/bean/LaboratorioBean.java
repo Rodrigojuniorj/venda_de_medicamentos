e27424848e481617cd15358d6959a16f4ec079fc
wesleyfantineli <88910865+wesleyfantineli@users.noreply.github.com>
2021-11-19 01:14:28 -0300

    LABORATORIO-PRONTO
    Todos os crud inserido

--- src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/LaboratorioBean.java
+++ src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/LaboratorioBean.java
@@ -1,6 +1,7 @@
 package br.edu.unifio.nosso.grupo.venda_de_medicamentos.bean;
 
 import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Laboratorio;
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Lote;
 import br.edu.unifio.nosso.grupo.venda_de_medicamentos.repository.LaboratorioRepository;
 import lombok.Data;
 import org.omnifaces.util.Faces;
@@ -43,11 +44,24 @@ public class LaboratorioBean {
     public void selecionarEdicao(Laboratorio laboratorio){
         try {
             Faces.setFlashAttribute("laboratorio", laboratorio);
-            Faces.redirect("laboratorio-edicao.xhtml");
+            Faces.redirect("laboratorio-editar.xhtml");
         } catch (Exception e) {
 
         }
     }
+    public void selecionarExclusao(Laboratorio laboratorio){
+        Faces.setFlashAttribute("laboratorio", laboratorio);
+        Faces.navigate("laboratorio-exclusao.xhtml?faces-redirect=true");
+    }
+
+    public void carreg(){
+        laboratorio = Faces.getFlashAttribute("laboratorio");
+
+        if(laboratorio == null){
+            Faces.navigate("laboratorio-lista.xhtml?faces-redirect=true");
+        }
+    }
+
     public void retornarEdicao(){
         if (Faces.getFlashAttribute("laboratorio") != null) {
             laboratorio = Faces.getFlashAttribute("laboratorio");
@@ -64,6 +78,8 @@ public class LaboratorioBean {
         try{
             laboratorioRepository.deleteById(laboratorio.getCodigo());
             Messages.addFlashGlobalInfo("Laboratorio removido com sucesso.");
+
+            Faces.navigate("laboratorio-listar.xhtml?faces-redirect=true");
         }catch(DataIntegrityViolationException e){
             Messages.addGlobalWarn("Erro: o registro selecionado está vinculado com outros registros.");
         }
