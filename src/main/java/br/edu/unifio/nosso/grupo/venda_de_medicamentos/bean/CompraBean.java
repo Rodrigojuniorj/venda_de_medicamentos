176343ac792b54d0671001d33849b0a0e030874c
João Walter Amadeu <joaowa2002@live.com>
2021-11-21 18:11:21 -0300

    Adicionado Beans para compra e pagamento

--- src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/CompraBean.java
+++ src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/CompraBean.java
@@ -1,4 +1,75 @@
 package br.edu.unifio.nosso.grupo.venda_de_medicamentos.bean;
 
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Compra;
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Pagamento;
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.repository.CompraRepository;
+import lombok.Data;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import javax.faces.view.ViewScoped;
+import java.util.List;
+
+@Component
+@ViewScoped
+@Data
 public class CompraBean {
+    private Compra compra;
+    private List<Compra> compras;
+
+    @Autowired
+    private CompraRepository compraRepository;
+
+    public void novaCompra(){
+        compra = new Compra();
+    }
+    public void salvarCompra(){
+        try{
+            compraRepository.save(compra);
+            Messages.addGlobalInfo("Compra salva com sucesso!");
+        }catch (DataIntegrityViolationException e){
+            Messages.addGlobalWarn("Erro: Já existe uma compra cadastrada com esse código.");
+        }
+    }
+    public void listarCompra(){
+        compras = compraRepository.findAll(Sort.by(Sort.Direction.ASC, "horario"));
+    }
+    public void selecionarEdicao(Compra compra) {
+        try {
+            Faces.setFlashAttribute("compra", compra);
+            Faces.redirect("compra-editar.xhtml");
+        } catch (Exception e) {
+
+        }
+
+    }
+    public void retornarEdicao() {
+        if (Faces.getFlashAttribute("compra") != null) {
+            compra = Faces.getFlashAttribute("compra");
+        } else {
+            try{
+                Faces.redirect("compra-lista.xhtml");
+                Messages.addGlobalInfo("Nenhuma compra selecionada!");
+            }catch (Exception e){
+
+            }
+        }
+    }
+    public void excluirCompra(){
+        try{
+            compraRepository.deleteById(compra.getCodigo());
+            Messages.addFlashGlobalInfo("Compra removida com sucesso.");
+        }catch(DataIntegrityViolationException e){
+            Messages.addGlobalWarn("Erro: o registro selecionado está vinculado com outros registros.");
+        }
+    }
+    public void selecionarExclusao(Compra compra){
+        Faces.setFlashAttribute("compra", compra);
+        Faces.navigate("compra-exclusao.xhtml?faces-redirect=true");
+    }
+
 }
--- src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/PagamentoBean.java
+++ src/main/java/br/edu/unifio/nosso/grupo/venda_de_medicamentos/bean/PagamentoBean.java
@@ -2,8 +2,8 @@ package br.edu.unifio.nosso.grupo.venda_de_medicamentos.bean;
 
 
 
-import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Laboratorio;
-import br.edu.unifio.nosso.grupo.venda_de_medicamentos.repository.LaboratorioRepository;
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.domain.Pagamento;
+import br.edu.unifio.nosso.grupo.venda_de_medicamentos.repository.PagamentoRepository;
 import lombok.Data;
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
@@ -21,7 +21,62 @@ import java.util.List;
 @Data
 
 public class PagamentoBean {
+    private Pagamento pagamento;
+    private List<Pagamento> pagamentos;
 
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    public void novoPagamento(){
+        pagamento = new Pagamento();
+    }
+
+    public void salvarPagamento(){
+        try{
+            pagamentoRepository.save(pagamento);
+            Messages.addGlobalInfo("Pagamento salvo com sucesso!");
+        }catch (DataIntegrityViolationException e){
+            Messages.addGlobalWarn("Erro: Já existe um Pagamento cadastrado com esse código.");
+        }
+    }
+    public void listarPagamento(){
+        pagamentos = pagamentoRepository.findAll(Sort.by(Sort.Direction.ASC, "horario"));
+    }
+
+    public void selecionarEdicao(Pagamento pagamento) {
+        try {
+            Faces.setFlashAttribute("pagamento", pagamento);
+            Faces.redirect("pagamento-editar.xhtml");
+        } catch (Exception e) {
+
+        }
+
+    }
+    public void retornarEdicao() {
+        if (Faces.getFlashAttribute("pagamento") != null) {
+            pagamento = Faces.getFlashAttribute("pagamento");
+        } else {
+            try{
+                Faces.redirect("pagamento-lista.xhtml");
+                Messages.addGlobalInfo("Nenhum pagamento selecionado!");
+            }catch (Exception e){
+
+            }
+        }
+    }
+    public void excluirPagamento(){
+        try{
+            pagamentoRepository.deleteById(pagamento.getCodigo());
+            Messages.addFlashGlobalInfo("Pagamento removido com sucesso.");
+        }catch(DataIntegrityViolationException e){
+            Messages.addGlobalWarn("Erro: o registro selecionado está vinculado com outros registros.");
+        }
+    }
+
+    public void selecionarExclusao(Pagamento pagamento){
+        Faces.setFlashAttribute("pagamento", pagamento);
+        Faces.navigate("pagamento-exclusao.xhtml?faces-redirect=true");
+    }
 
 
 
